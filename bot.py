90fcec6583d95ae9272466c4624b13ab4e7c9a5c
thant-zinoo <158450945+thant-zinoo@users.noreply.github.com>
2024-02-22 10:10:58 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ PREFIX = "!"
 async def echo(room, message):
     match = botlib.MessageMatch(room, message, bot, PREFIX)
     if match.prefix():
-        # if match.is_not_from_this_bot():
+        # if match.prefix() and match.is_not_from_this_bot():
 
         if match.command("binance"):
             try:
